d8ada037537dca1ae326beed1ed874aed4aa3aba
Amr Aly <amr.aly57@yahoo.com>
2018-09-26 07:41:26 +0200

    Init widgets and set excerpt length

--- functions.php
+++ functions.php
@@ -12,3 +12,19 @@ function theme_setup() {
 }
 
 add_action('after_setup_theme', 'theme_setup');
+
+function set_excerpt_length($length) {
+    return 10;
+}
+
+add_filter('excerpt_length', 'set_excerpt_length');
+
+// Widget Locations
+function init_widgets($id) {
+    register_sidebar([
+        'name' => 'Sidebar',
+        'id'   => 'sidebar'
+    ]);
+}
+
+add_action('widgets_init','init_widgets');
